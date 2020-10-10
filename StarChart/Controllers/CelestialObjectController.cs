91ec1f2fa22aaf99e658a73186d056db71ba9b27
Amit Dhakre <amitdhakre13@gmail.com>
2020-10-10 14:42:27 +0530

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +14,56 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext dbContext)
         {
             this._context = dbContext;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if(celestialObject == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                CelestialObject co = new CelestialObject();
+                co.Satellites = celestialObject.Satellites;
+
+                return Ok(celestialObject);
+            }
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(c => c.Name == name);
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                CelestialObject co = new CelestialObject();
+                co.Satellites = celestialObject.Satellites;
+
+                return Ok(celestialObject);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects;
+
+            foreach(var c in celestialObjects)
+            {
+
+            }
+            CelestialObject co = new CelestialObject();
+            //co.Satellites = celestialObject.Satellites;
+
+            return Ok(co);
+
         }
     }
 }
